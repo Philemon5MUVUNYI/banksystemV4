e609d34756450e76b7febb1d5843f65e04f7aa2e
Philemon5MUVUNYI <muvunyiphilemon22@gmail.com>
2025-10-14 16:00:04 +0200

    registry build

--- src/dao/BankAccountDao.java
+++ src/dao/BankAccountDao.java
@@ -1,6 +1,34 @@
 package dao;
 
+import model.BankAccount;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class BankAccountDao {
+    private String jdUrl ="jdbc:postgresql://localhost:5432/javathursdayv4";
+    private String jdUserName ="postgres";
+    private String jdPassword ="root";
 
+    public Integer registerDetails(BankAccount account_dets){
+        try {
+            Connection con_register = DriverManager.getConnection(jdUrl ,jdUserName ,jdPassword);
+            String sql = "INSERT INTO accounts(number ,name ,type ,amount) values (? ,? ,? ,?)";
 
+            PreparedStatement pst_register =con_register.prepareStatement(sql);
+            pst_register.setString(1,account_dets.getAccountNumber());
+            pst_register.setString(2, account_dets.getAccountName());
+            pst_register.setString(3,account_dets.getAccountType());
+            pst_register.setDouble(4,account_dets.getAmount());
+
+            int rowsAffected = pst_register.executeUpdate();
+
+            con_register.close();
+            return rowsAffected;
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return 0;
+    }
 }
--- src/view/CoreBanking.java
+++ src/view/CoreBanking.java
@@ -1,18 +1,79 @@
 package view;
 
+import dao.BankAccountDao;
+import model.BankAccount;
+
+import java.util.Scanner;
+
 public class CoreBanking {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        String AccountName;
-        String AccountNumber;
-        String AccountType;
+        String accountName;
+        String accountNumber;
+        String accountType;
         double amount;
 
         boolean condition  = true;
 
         while (condition){
-            System.out.println("--------------------------");
-            condition = false;
+            System.out.println("------------------------------------");
+            System.out.println("===Menu===");
+            System.out.println("1. Register account.");
+            System.out.println("2. Display all accounts.");
+            System.out.println("3. Update accounts.");
+            System.out.println("4. Search account.");
+            System.out.println("5. Delete account.");
+            System.out.println("0 .Exit.");
+            System.out.println("======================================");
+            System.out.print("Enter your choice: ");
+            Integer choice = input.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("=======================================");
+                    System.out.print("Create your account number: ");
+                    accountNumber = input.next();
+                    System.out.print("Enter your name: ");
+                    accountName = input.next();
+                    System.out.print("Enter the type of account: ");
+                    accountType = input.next();
+                    System.out.print("Enter the amount: ");
+                    amount = input.nextDouble();
+
+                    //shifting data to model
+                    BankAccount account_info = new BankAccount();
+                    account_info.setAccountNumber(accountNumber);
+                    account_info.setAccountName(accountName);
+                    account_info.setAccountType(accountType);
+                    account_info.setAmount(amount);
+
+                    //Putting the data to dao
+                    BankAccountDao dao_registry = new BankAccountDao();
+                    int rowsAffected = dao_registry.registerDetails(account_info);
+                    if (rowsAffected > 0){
+                        System.out.println("===========================");
+                        System.out.println("Data saved successfully.");
+                    }else {
+                        System.out.println("Nothing happened");
+                    }
+                    break;
+                case 2:
+                    System.out.println("");
+                    break;
+                case 3:
+                    System.out.println("");
+                    break;
+                case 4:
+                    System.out.println("");
+                    break;
+                case 5:
+                    System.out.println("");
+                    break;
+                case 0:
+                    System.out.println("Thank you for using this system.");
+                    System.exit(0);
+            }
         }
     }
 }
