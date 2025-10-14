4f9aff613d98335f1db78834826591e683a56a61
Philemon5MUVUNYI <muvunyiphilemon22@gmail.com>
2025-10-14 14:33:39 +0200

    Adding classes

--- src/model/BankAccount.java
+++ src/model/BankAccount.java
@@ -1,4 +1,54 @@
 package model;
 
 public class BankAccount {
+    private String accountName;
+    private String accountNumber;
+    private String accountType;
+    private double amount;
+
+    public BankAccount( String accountName ,String accountNumber ,String accountType ,double amount){
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.amount = amount;
+    }
+    public BankAccount(){
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.amount = amount;
+    }
+
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
--- src/view/CoreBanking.java
+++ src/view/CoreBanking.java
@@ -1,4 +1,18 @@
 package view;
 
 public class CoreBanking {
+    public static void main(String[] args) {
+
+        String AccountName;
+        String AccountNumber;
+        String AccountType;
+        double amount;
+
+        boolean condition  = true;
+
+        while (condition){
+            System.out.println("--------------------------");
+            condition = false;
+        }
+    }
 }
