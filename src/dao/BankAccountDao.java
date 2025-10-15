618ff7bafd525b81ca5a71fedfda63b2100e7434
Philemon5MUVUNYI <muvunyiphilemon22@gmail.com>
2025-10-15 09:42:44 +0200

    Display build

--- src/dao/BankAccountDao.java
+++ src/dao/BankAccountDao.java
@@ -1,10 +1,13 @@
 package dao;
 
 import model.BankAccount;
+import view.CoreBanking;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class BankAccountDao {
     private String jdUrl ="jdbc:postgresql://localhost:5432/javathursdayv4";
@@ -30,5 +33,29 @@ public class BankAccountDao {
             ex.printStackTrace();
         }
         return 0;
+    }
+    public void displayAllData(){
+
+        Scanner input1 = new Scanner(System.in);
+        CoreBanking core = new CoreBanking();
+        try {
+            Connection con_display = DriverManager.getConnection(jdUrl ,jdUserName ,jdPassword);
+            String sql = "Select * from accounts";
+            PreparedStatement pst_display = con_display.prepareStatement(sql);
+            ResultSet rs_display = pst_display.executeQuery();
+            int count = 0;
+            while (rs_display.next()){
+                count++;
+                System.out.println("  Account "+count);
+                System.out.println("Number: "+rs_display.getString("number"));
+                System.out.println("Name  : "+rs_display.getString("name"));
+                System.out.println("Type  : "+rs_display.getString("type"));
+                System.out.println("Amount: "+rs_display.getString("amount"));
+                System.out.println("\n");
+            }
+            con_display.close();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
     }
 }
